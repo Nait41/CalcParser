f0819e0b3ac7854e570076936b64d933d70dc89f
Nait <floop.41@mail.ru>
2022-06-25 17:14:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        MainFormGUI mainFormGUI = new MainFormGUI();
+    }
+}
---
+++ src/OpenDocument.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import java.awt.FileDialog;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.table.DefaultTableModel;
+public class OpenDocument extends JFrame{
+    String path;
+    public OpenDocument() throws IOException {
+        FileDialog Open = new FileDialog(this, null, FileDialog.LOAD);
+        Open.setFile("*.TXT");
+        Open.setVisible(true);
+        String NameFile = Open.getDirectory() + Open.getFile();
+        path = NameFile;
+    }
+}
